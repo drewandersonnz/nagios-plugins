98aae06ca5dea75710b4fdb219b61b9f718d2de5
Drew Anderson <d.anderson@beovista.com.au>
2016-01-06 15:56:44 +1000

    changed

--- generate_domain_expiry.py
+++ generate_domain_expiry.py
@@ -10,15 +10,38 @@ import argparse
 
 args = argparse.ArgumentParser()
 args.add_argument("domain", help="domain to check")
+args.add_argument("--ip", help="IP domain must report as", default=None)
 args = args.parse_args()
 
-print("""
-
-define service {
+check_command = "check_domain_expiry"
+print("""define service {
     host_name               domains.fakehost
-    service_description     domain %s
-    check_command           check_domain_expiry!%s
+    service_description     %(domain)s domain expiry
+    check_command           %(check_command)s!%(domain)s
     use                     generic-service
     notification_interval   0 ; set > 0 if you want to be renotified
 }
-""" % (args.domain, args.domain))
+""" % dict(domain=args.domain, check_command=check_command))
+
+check_command = "check_domain_response_ip"
+if args.ip:
+    check_command = "check_domain_response_ip_match"
+
+print("""define service {
+    host_name               domains.fakehost
+    service_description     %(domain)s domain ip
+    check_command           %(check_command)s!%(domain)s
+    use                     generic-service
+    notification_interval   0 ; set > 0 if you want to be renotified
+}
+""" % dict(domain=args.domain, check_command=check_command))
+print("""define service {
+    host_name               domains.fakehost
+    service_description     %(domain)s domain ip www
+    check_command           %(check_command)s!www%(domain)s
+    use                     generic-service
+    notification_interval   0 ; set > 0 if you want to be renotified
+}
+""" % dict(domain=args.domain, check_command=check_command))
+
+print()
