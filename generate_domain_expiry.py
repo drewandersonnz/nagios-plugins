ce802be2ef119bf616f2751fef19493fb0711aa1
Drew Anderson <drew@onlycentral.com>
2016-01-06 20:55:24 +1000

    tweaks to support implementation

--- generate_domain_expiry.py
+++ generate_domain_expiry.py
@@ -15,33 +15,30 @@ args = args.parse_args()
 
 check_command = "check_domain_expiry"
 print("""define service {
+    use                     check-domain-expiry-parent
     host_name               domains.fakehost
     service_description     %(domain)s domain expiry
     check_command           %(check_command)s!%(domain)s
-    use                     generic-service
-    notification_interval   0 ; set > 0 if you want to be renotified
 }
-""" % dict(domain=args.domain, check_command=check_command))
+""" % dict(domain=args.domain, check_command=check_command, ip=args.ip))
 
 check_command = "check_domain_response_ip"
 if args.ip:
     check_command = "check_domain_response_ip_match"
 
 print("""define service {
+    use                     check-domain-expiry-parent
     host_name               domains.fakehost
     service_description     %(domain)s domain ip
-    check_command           %(check_command)s!%(domain)s
-    use                     generic-service
-    notification_interval   0 ; set > 0 if you want to be renotified
+    check_command           %(check_command)s!%(domain)s!%(ip)s
 }
-""" % dict(domain=args.domain, check_command=check_command))
+""" % dict(domain=args.domain, check_command=check_command, ip=args.ip))
 print("""define service {
+    use                     check-domain-expiry-parent
     host_name               domains.fakehost
     service_description     %(domain)s domain ip www
-    check_command           %(check_command)s!www%(domain)s
-    use                     generic-service
-    notification_interval   0 ; set > 0 if you want to be renotified
+    check_command           %(check_command)s!www.%(domain)s!%(ip)s
 }
-""" % dict(domain=args.domain, check_command=check_command))
+""" % dict(domain=args.domain, check_command=check_command, ip=args.ip))
 
 print()
